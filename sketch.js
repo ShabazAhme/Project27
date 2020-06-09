2d9b3354ea3a8c45d0f8e9ac8cb2f4a45caa68e1
ShabazAhme <64406126+ShabazAhme@users.noreply.github.com>
2020-06-09 10:30:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,64 +1,44 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Render = Matter.Render;
-const Constraint = Matter.Constraint;
-function preload()
-{
-	
+var dust;
+var engine,world;
+let img;
+function preload(){
+    img = loadImage("dustbingreen.png");
+    img.scale=0.5;
+    imageMode(CENTER);
 }
 
 function setup() {
-	createCanvas(800, 800);
-
-    engine = Engine.create();
-	  world = engine.world;
- 
-    roof = new Roof(400,100,700,50);	
-
-	  bobObject1 = new Bob(300,400,50,50);
-    bobObject2 = new Bob(350,400,50,50);
-    bobObject3 = new Bob(400,400,50,50);
-    bobObject4 = new Bob(450,400,50,50);
-    bobObject5 = new Bob(500,400,50,50);                                                                       
-    rope1 = new Rope(bobObject1.body,roof.body,0,0);
-    rope2 = new Rope(bobObject2.body,roof.body,0,0);
-    rope3 = new Rope(bobObject3.body,roof.body,0,0);
-    rope4 = new Rope(bobObject4.body,roof.body,0,0);
-    rope5 = new Rope(bobObject5.body,0 ,500,100);
-    
-    Engine.run(engine);
   
-}
-
-           
-function draw() {
-  rectMode(CENTER);
-  background("white");
+   var canvas = createCanvas(900,600);
+   engine = Engine.create();
+   world = engine.world;   
    
-  roof.display();
-
-  bobObject1.display();
-  bobObject2.display();
-  bobObject3.display();
-  bobObject4.display();
-  bobObject5.display();
- 
-  rope1.display();
-  rope2.display();
-  rope3.display();
-  rope4.display();
-  rope5.display();
-  drawSprites();
- 
+   ground = new Ground(450,595,900,10);
+   
+   paper = new Paper(200,200,50,50);
+   bin1 = new Bin(800,450,10,100);
+   
 }
 
-function keyPressed(){
-	if(keyCode == UP_ARROW){
-		Matter.Body.applyForce(bobObject5.body,bobObject5.body.position,{x:85,y:-85})
-	}
+
+function draw() {
+    background("white");
+    Engine.update(engine);
+
+    ground.display();
+    bin1.display();
+       
+    paper.display();
+    image(img,650,450);
 }
 
+
+function keyReleased() {
+    if(keyCode===32){
+        paper.body.position.x=-2;
+        paper.body.position.y=4;
+    }
+}
